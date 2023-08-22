49f2f11668bb4d591ab9450748219d4ab24c696d
Margit Tennosaar <46651865+margittennosaar@users.noreply.github.com>
2023-08-22 14:40:16 +0300

    add solutions from Tuesday

--- task2/main.js
+++ task2/main.js
@@ -44,3 +44,42 @@ The salary is calculated as follows:
  - first 7 hours by hourly salary
  - next 2 hours, 50% extra
  - rest of the hours 100% extra */
+
+const hourlySalary = parseFloat(prompt("Enter your hourly salary:"));
+const hoursWorked = parseFloat(prompt("Enter the number of hours worked in a day:"));
+
+let totalSalary = 0;
+
+if (hoursWorked <= 7) {
+  totalSalary = hourlySalary * hoursWorked;
+} else if (hoursWorked <= 9) {
+  totalSalary = (hourlySalary * 7) + (hourlySalary * 1.5 * (hoursWorked - 7));
+} else {
+  totalSalary = (hourlySalary * 7) + (hourlySalary * 1.5 * 2) + (hourlySalary * 2 * (hoursWorked - 9));
+}
+
+alert("Your day's salary is: $" + totalSalary.toFixed(2));
+
+
+/* Task - Random Number 
+ Make a program that asks for three numbers from the user. The program prints out the sum of the numbers if any of the numbers are positive. If all the numbers are positive, also print out multiplication. If all numbers are negative, the text “only negatives” is printed. Handle zero as positive in this assignment. */
+
+const randomNumbers = () => {
+  const num1 = Number(prompt('Please give first number'));
+  const num2 = Number(prompt('Please give second number'));
+  const num3 = Number(prompt('Please give third number'));
+
+  const mult = num1 * num2 * num3;
+  const sum = num1 + num2 + num3;
+
+  if (num1 >= 0 && num2 >= 0 && num3 >= 0) {
+    console.log('sum is: ' + sum + ' and multiplication is: ' + mult)
+  } else if (num1 <= 0 || num2 <= 0 || num3 <= 0) {
+    if (num1 <= 0 && num2 <= 0 && num3 <= 0) {
+      console.log('all numbers are negative');
+    } else {
+      console.log('sum is:', sum);
+    }
+  }
+}
+randomNumbers() 
