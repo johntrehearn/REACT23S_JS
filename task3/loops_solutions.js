b545cf700fc8af220baf3a9313568f066a9d6c0c
Margit Tennosaar <46651865+margittennosaar@users.noreply.github.com>
2023-08-28 16:21:38 +0300

    add all solution for loop task

--- task3/loops_solutions.js
+++ task3/loops_solutions.js
@@ -73,12 +73,90 @@ console.log(sum / (count - 1));
 
 /* 6. Make a program that asks 25 numbers form the user. In the end program prints out average of the numbers. */
 
+let sum = 0;
+
+for (let i = 0; i < 25; i++) {
+  const input = parseFloat(prompt(`Enter number ${i + 1}:`));
+  sum += number;
+}
+
+const average = sum / 25;
+console.log(`Average of the entered numbers: ${average}`);
 
 
 /* 7. Make a program that ask first one number from the user. After that the program asks: ”Do you want to continue giving numbers?(y/n)”. If user answers y, the program continues to ask another number. If user answers n, program ends. In the end program prints out average of the numbers. */
 
+let sum = 0;
+let count = 0;
+
+while (true) {
+  const input = parseFloat(prompt("Enter a number:"));
+
+  sum += input;
+  count++;
+
+  const shouldContinue = prompt("Do you want to continue giving numbers? (y/n)").toLowerCase();
+  if (shouldContinue !== 'y') {
+    break;
+  }
+}
+console.log(`Average of the entered numbers: ${sum / count}`);
+
+
 /* 8. Make a program that asks first how many numbers user wants to give to the program. After that program asks those numbers. In the end program prints out the smallest number that user gave. */
+
+let smallestNumber = 0;
+let inputAmount = parseInt(prompt("How many numbers do you want to provide?"));
+
+for (let i = 0; i < inputAmount; i++) {
+  const inputNumber = parseInt(prompt(`Enter number ${i + 1}:`));
+
+  if (inputNumber < smallestNumber) {
+    smallestNumber = inputNumber;
+  }
+}
+console.log(`The smallest number among the provided numbers: ${smallestNumber}`)
 
 // 9. Make a program that asks ten numbers and in the end prints out two biggest numbers.
 
+// Initialize variables to store the two biggest numbers
+let biggest1 = -Infinity;
+let biggest2 = -Infinity;
+
+for (let i = 0; i < 10; i++) {
+  const input = parseFloat(prompt(`Enter number ${i + 1}:`));
+
+  if (input > biggest1) {
+    biggest2 = biggest1;
+    biggest1 = input;
+  } else if (input > biggest2) {
+    biggest2 = input;
+  }
+}
+
+console.log(`The two biggest numbers: ${biggest1} and ${biggest2}`);
+
+
 // 10. Make a program that asks ten numbers. Program calculates and prints out sum and average, also prints out the smallest and biggest number.
+
+let sum = 0;
+let smallest = Infinity;
+let biggest = -Infinity;
+
+for (let i = 0; i < 10; i++) {
+  const input = parseFloat(prompt(`Enter number ${i + 1}:`));
+
+  sum += input;
+
+  if (input < smallest) {
+    smallest = input;
+  }
+
+  if (input > biggest) {
+    biggest = input;
+  }
+}
+
+const average = sum / 10;
+
+console.log(`Sum of the numbers: ${sum}. Average of the numbers: ${average}. Smallest number: ${smallest}. Biggest number: ${biggest}`);
