5602c45060157d4e0aef63b498bd9aa7fd9b7467
Margit Tennosaar <46651865+margittennosaar@users.noreply.github.com>
2023-09-19 13:11:04 +0300

    header background

--- tricks/main.js
+++ tricks/main.js
@@ -1,6 +1,8 @@
 const backButton = document.querySelector('#backToTop');
 const mobButton = document.querySelector('.mobile')
 const nav = document.querySelector('nav ul')
+const menuItems = document.querySelectorAll('nav ul li a')
+const header = document.querySelector('header')
 
 // browser onscroll event triger
 window.onscroll = function () { scrollFunction() };
@@ -12,6 +14,11 @@ function scrollFunction() {
   } else {
     backButton.style.display = "none";
   }
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    header.classList.add('bg')
+  } else {
+    header.classList.remove('bg')
+  }
 }
 
 const getToTop = () => {
@@ -19,6 +26,10 @@ const getToTop = () => {
   document.documentElement.scrollTop = 0;
 }
 const mobMenu = () => {
+  for (const item of menuItems) {
+    item.addEventListener('click', mobMenu)
+  }
+
   if (nav.classList.contains('responsive')) {
     nav.classList.remove('responsive')
   } else {
