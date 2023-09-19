ce136c469e89f66dff1c11dc966c0aa0ff1f1b40
Margit Tennosaar <46651865+margittennosaar@users.noreply.github.com>
2023-09-19 12:55:48 +0300

    add mobile menu

--- tricks/main.js
+++ tricks/main.js
@@ -1,7 +1,11 @@
 const backButton = document.querySelector('#backToTop');
+const mobButton = document.querySelector('.mobile')
+const nav = document.querySelector('nav ul')
 
+// browser onscroll event triger
 window.onscroll = function () { scrollFunction() };
 
+// code form W3S about scrolling, for for two browsers
 function scrollFunction() {
   if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
     backButton.style.display = "block";
@@ -14,5 +18,14 @@ const getToTop = () => {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+const mobMenu = () => {
+  if (nav.classList.contains('responsive')) {
+    nav.classList.remove('responsive')
+  } else {
+    nav.classList.add('responsive')
+  }
+
+}
 
 backButton.addEventListener('click', getToTop)
+mobButton.addEventListener('click', mobMenu)
