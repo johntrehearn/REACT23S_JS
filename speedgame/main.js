8e2996cc779dde9b31ec9ceb51fd7cf9a27c622b
Margit Tennosaar <46651865+margittennosaar@users.noreply.github.com>
2023-09-26 13:49:46 +0300

    MVP ready (missing modal and conditional start/end button; conditional message in end of game

--- speedgame/main.js
+++ speedgame/main.js
@@ -5,6 +5,10 @@ const scoreDisplay = document.querySelector('.score')
 
 // global variables
 let score = 0;
+let timer;
+let pace = 1000;
+let active = 0;
+let rounds = 0;
 
 // code from W3S page for the random number 
 /* function getRndInt(min, max) {
@@ -14,7 +18,10 @@ let score = 0;
 const getRndInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 const clickCircle = (i) => {
-  console.log('circle was clicked', i);
+  if (i !== active) {
+    return endGame()
+  }
+  rounds--
   score += 10
   scoreDisplay.textContent = score
 }
@@ -28,12 +35,46 @@ circles.forEach((circle, i) => {
   item.addEventListener('click', () => clickCircle(i))
 } */
 
+const enableEvents = () => {
+  circles.forEach(circle => {
+    circle.style.pointerEvents = "auto"
+  })
+}
+
 const startGame = () => {
-  console.log('game started');
+  if (rounds >= 3) {
+    return endGame()
+  }
+
+  enableEvents();
+  const newActive = pickNew(active)
+
+  circles[newActive].classList.toggle('active')
+  circles[active].classList.remove('active')
+
+  active = newActive;
+
+  timer = setTimeout(startGame, pace)
+
+  pace -= 10
+  rounds++
+  function pickNew(active) {
+    const newActive = getRndInt(0, 3)
+    if (newActive !== active) {
+      return newActive
+    }
+    return pickNew(active)
+  }
 }
 
 const endGame = () => {
   console.log('game ended');
+  clearTimeout(timer);
+  resetGame();
+}
+
+const resetGame = () => {
+  window.location.reload()
 }
 
 startButton.addEventListener('click', startGame)
